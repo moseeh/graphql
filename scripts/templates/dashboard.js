eb5ab182954d8d2aea97cca0420b762a043be258
moonyango <mosesadrian825@gmail.com>
2025-03-06 18:53:35 +0300

    feat: add dashboard template

---
+++ scripts/templates/dashboard.js
@@ -0,0 +1,35 @@
+export const dashboard = () => `
+
+    <div>
+        <h1>Student Profile</h1>
+        <button id="logout-btn">Logout</button>
+    </div>
+   <section id="user-info" class="profile-section">
+            <h2>User Information</h2>
+            <div id="user-data">Loading...</div>
+        </section>
+        
+        <section id="xp-section" class="profile-section">
+            <h2>Experience Points</h2>
+            <div id="xp-data">Loading...</div>
+        </section>
+        
+        <section id="progress-section" class="profile-section">
+            <h2>Progress</h2>
+            <div id="progress-data">Loading...</div>
+        </section>
+        
+        <section id="statistics-section" class="profile-section">
+            <h2>Statistics</h2>
+            <div class="graph-container">
+                <h3>XP Over Time</h3>
+                <svg id="xp-time-graph" width="600" height="400"></svg>
+            </div>
+            
+            <div class="graph-container">
+                <h3>Project Pass/Fail Ratio</h3>
+                <svg id="pass-fail-graph" width="600" height="400"></svg>
+            </div>
+        </section>
+
+`;
