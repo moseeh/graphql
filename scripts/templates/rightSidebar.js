a6863bebf373c4a2eca74893f5d1b917a39d57c8
moonyango <mosesadrian825@gmail.com>
2025-03-15 15:46:03 +0300

    feat: functions returning html for skills and stats

---
+++ scripts/templates/rightSidebar.js
@@ -0,0 +1,64 @@
+export const stats = () => `
+    <div class="card">
+  <div class="card-header">
+    <div class="card-title">Course Statistics</div>
+    <div></div>
+  </div>
+  <div class="progress-list">
+    <div class="progress-item">
+      <div class="progress-label">Done</div>
+      <div class="progress-bar">
+        <div class="progress-fill" style="width: 65%"></div>
+      </div>
+    </div>
+    <div class="progress-item">
+      <div class="progress-label">On Progress</div>
+      <div class="progress-bar">
+        <div class="progress-fill" style="width: 45%"></div>
+      </div>
+    </div>
+
+    <div class="progress-item">
+      <div class="progress-label">To Do</div>
+      <div class="progress-bar">
+        <div class="progress-fill" style="width: 25%"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+`;
+
+export const skills = () => `
+<div class="card">
+  <div class="card-header">
+    <div class="card-title">Best Skills</div>
+  </div>
+
+  <ul class="topic-list">
+    <li class="topic-item">
+      <div class="topic-info">
+        <div class="topic-number">01</div>
+        <div class="topic-name">Cyber security basics</div>
+      </div>
+      <div class="topic-score high">92%</div>
+    </li>
+    <li class="topic-item">
+      <div class="topic-info">
+        <div class="topic-number">02</div>
+        <div class="topic-name">Covid protocols</div>
+      </div>
+      <div class="topic-score high">95%</div>
+    </li>
+    <li class="topic-item">
+      <div class="topic-info">
+        <div class="topic-number">03</div>
+        <div class="topic-name">Social media policies</div>
+      </div>
+      <div class="topic-score high">89%</div>
+    </li>
+  </ul>
+</div>
+`;
+
+// export const
