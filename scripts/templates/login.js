dce71b1e53c40910c1b2e0b6160ea051f0941e4a
moonyango <mosesadrian825@gmail.com>
2025-03-17 23:55:44 +0300

    fix: modify login form

--- scripts/templates/login.js
+++ scripts/templates/login.js
@@ -1,16 +1,15 @@
 export const login = () => `
-<div id="login-container">
-        <div id="error-message"></div>
-        <h2>Login </h2>
-        <form id="login-form" action="/submit-login" method="post">
-            <div class="form-group">
-                <input type="text" id="username" name="username" placeholder="username" required />
-            </div>
-            <div class="form-group">
-                <input type="password" id="password" name="password" placeholder="password" required>
-            </div>
-            <button type="submit"> Login</button>
-        </form>
-    </div>
-    
+  <div id="login-container">
+    <div id="error-message"></div>
+    <h2>Welcome Back</h2>
+    <form id="login-form">
+      <div class="form-group">
+        <input type="text" id="username" name="username" placeholder="Username or email " required />
+      </div>
+      <div class="form-group">
+        <input type="password" id="password" name="password" placeholder="Password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
 `;
