500e36533668c684f01911e95cf0ed47773075a0
moonyango <mosesadrian825@gmail.com>
2025-03-17 13:38:39 +0300

    fix: remove unused template

--- scripts/templates/rightSidebar.js
+++ scripts/templates/rightSidebar.js
@@ -1,34 +1,3 @@
-export const stats = () => `
-    <div class="card">
-  <div class="card-header">
-    <div class="card-title">Course Statistics</div>
-    <div></div>
-  </div>
-  <div class="progress-list">
-    <div class="progress-item">
-      <div class="progress-label">Done</div>
-      <div class="progress-bar">
-        <div class="progress-fill" style="width: 65%"></div>
-      </div>
-    </div>
-    <div class="progress-item">
-      <div class="progress-label">On Progress</div>
-      <div class="progress-bar">
-        <div class="progress-fill" style="width: 45%"></div>
-      </div>
-    </div>
-
-    <div class="progress-item">
-      <div class="progress-label">To Do</div>
-      <div class="progress-bar">
-        <div class="progress-fill" style="width: 25%"></div>
-      </div>
-    </div>
-  </div>
-</div>
-
-`;
-
 export const skills = () => `
 <div class="card">
   <div class="card-header">
@@ -38,5 +7,3 @@ export const skills = () => `
   <ul id="topic-list" class="topic-list"></ul>
 </div>
 `;
-
-// export const
